845944ee0b606b3add366c802c78a81bd4602b88
ylebovits <yakirlebovits@temple.edu>
2023-07-25 01:24:51 -0400

    fix typo

--- process_data.py
+++ process_data.py
@@ -19,14 +19,15 @@ def orchestrate_processing(output_filename: str) -> bool:
     land_gdf = get_arcgis_dataset(ARCGIS_LAND_DATASET_PATH)
     buildings_gdf = get_arcgis_dataset(ARCGIS_BUILDINGS_DATASET_PATH)
     phs_landcare_gdf = get_arcgis_dataset(ARCGIS_PHS_LANDCARE_DATASET_PATH)
-    # phs_maintenance_gdf = get_arcgis_dataset(ARCGIS_PHS_MAINTENANCE_DATASET_PATH)
-    # rco_gdf = get_arcgis_dataset(ARCGIS_RCO_DATASET_PATH)
-    # complaints_gdf = get_philadelphia_li_complaint_dataset()
-    # violations_gdf = get_philadelphia_li_violation_dataset()
+    phs_maintenance_gdf = get_arcgis_dataset(ARCGIS_PHS_MAINTENANCE_DATASET_PATH)
+    rco_gdf = get_arcgis_dataset(ARCGIS_RCO_DATASET_PATH)
+    complaints_gdf = get_philadelphia_li_complaint_dataset()
+    violations_gdf = get_philadelphia_li_violation_dataset()
 
     # preprocess data
 
     # merge data
+    return True
 
 
 def main():
