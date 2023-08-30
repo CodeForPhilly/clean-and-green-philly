9d31875a80a0dcfef84b8053e43bfdc2554d99ef
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-08-30 10:21:29 -0400

    black formatting

--- src/script.py
+++ src/script.py
@@ -58,7 +58,7 @@ vacant_properties = gun_crimes(vacant_properties)
 Post to Mapbox
 """
 vacant_properties.drop_columns()
-vacant_properties.upload_to_mapbox('vacant_properties')
+vacant_properties.upload_to_mapbox("vacant_properties")
 
 # Clean up
 vacant_properties.gdf.to_postgis(
