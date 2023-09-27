a5940d4325110c9439af4a375060fe91450a23cb
nlebovits <nissim.lebovits@vanderbilt.edu>
2023-09-27 15:24:36 -0400

    add basic actions page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,6 +10,7 @@ import {
   MapIcon,
   QuestionMarkCircleIcon,
   InformationCircleIcon,
+  BoltIcon,
 } from "@heroicons/react/20/solid";
 
 const navbarButtons = [
@@ -24,9 +25,9 @@ const navbarButtons = [
     href: "/map",
   },
   {
-    icon: <QuestionMarkCircleIcon />,
-    text: "Help",
-    href: "#",
+    icon: <BoltIcon />,
+    text: "Take Action",
+    href: "/actions",
   },
   {
     icon: <InformationCircleIcon />,
---
+++ src/app/components/RecommendedActions.tsx
@@ -0,0 +1,42 @@
+import { Button, Link } from "@nextui-org/react";
+
+const RecommendedActions = () => (
+    <div className="container mx-auto pt-20">
+      <div className="text-left text-2xl my-10">
+        
+            Take Action
+            What can I do with this?
+
+            This project is mainly meant to support “cleaning and greening” to deter gun violence. According to the research that this dashboard is based on , that involves:
+
+                Removing trash and debris
+                Grading the land
+                Planting a small number of trees to create a park-like setting
+                Installing low-perimeter (about 1 meter high) fences with multiple ungated entrances to encourage use
+                Regularly maintaining the lot
+
+            This approach has been shown to be inexpensive and cost-effective, with costs averaging about $5 per square meter and maintenance averaging $0.50 per square meter thereafter. If you’re not able to take on a full intervention, it’s still worthwhile to install a fence, plant trees, or even simply keep the lot free of trash, which can be done on your own.
+            Where can I get help?
+
+            To carry out something like this, try getting in touch with your local Neighborhood Advisory Committee , a nearby Community Development Corporation , or your local representative . Explain to them what you want to do and why. Show them this map to support your explanation!
+
+            Be mindful of trespassing on private property. You can use the dashboard tool above to identify which lots are privately owned. Consider reaching out to a lot owner and explaining why you want to carry out a lot cleanup in order to get permission.
+
+            Lastly, you can also use these data to convince your NAC, CDC, elected official, or another organization to take action. If you are interested in getting specific data that were used to build this tool, contact me and let me know how I can help.
+            Overview
+            Decision Tree
+            Potential Uses for Parcels
+            Things to Consider
+            Funding
+            Long-Term Maintenance
+            Gentrification
+
+      </div>
+      <div className="flex justify-between space-x-2.5">
+
+      </div>
+
+    </div>
+);
+
+export default RecommendedActions;
