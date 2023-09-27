da8679721d2912235928dfae4a52712f514b4e2f
nlebovits <nissim.lebovits@vanderbilt.edu>
2023-09-27 16:27:25 -0400

    update actions styling

--- src/app/components/RecommendedActions.tsx
+++ src/app/components/RecommendedActions.tsx
@@ -1,42 +1,63 @@
 import { Button, Link } from "@nextui-org/react";
 
 const RecommendedActions = () => (
-    <div className="container mx-auto pt-20">
-      <div className="text-left text-2xl my-10">
-        
-            Take Action
-            What can I do with this?
+  <div className="container mx-auto pt-20 h-screen">
+    <div className="text-left text-2xl my-10">
+      <h1 className="text-3xl font-bold mb-4">Take Action</h1>
+      <p className="mb-4">What can I do with this?</p>
 
-            This project is mainly meant to support “cleaning and greening” to deter gun violence. According to the research that this dashboard is based on , that involves:
+      <ul className="list-disc pl-6 mb-6">
+        <li>Removing trash and debris</li>
+        <li>Grading the land</li>
+        <li>Planting a small number of trees to create a park-like setting</li>
+        <li>Installing low-perimeter (about 1 meter high) fences with multiple ungated entrances to encourage use</li>
+        <li>Regularly maintaining the lot</li>
+      </ul>
 
-                Removing trash and debris
-                Grading the land
-                Planting a small number of trees to create a park-like setting
-                Installing low-perimeter (about 1 meter high) fences with multiple ungated entrances to encourage use
-                Regularly maintaining the lot
+      <p className="mb-4">
+        This approach has been shown to be inexpensive and cost-effective, with costs averaging about $5 per square meter and maintenance averaging $0.50 per square meter thereafter. If you’re not able to take on a full intervention, it’s still worthwhile to install a fence, plant trees, or even simply keep the lot free of trash, which can be done on your own.
+      </p>
 
-            This approach has been shown to be inexpensive and cost-effective, with costs averaging about $5 per square meter and maintenance averaging $0.50 per square meter thereafter. If you’re not able to take on a full intervention, it’s still worthwhile to install a fence, plant trees, or even simply keep the lot free of trash, which can be done on your own.
-            Where can I get help?
+      <p className="mb-4">Where can I get help?</p>
 
-            To carry out something like this, try getting in touch with your local Neighborhood Advisory Committee , a nearby Community Development Corporation , or your local representative . Explain to them what you want to do and why. Show them this map to support your explanation!
+      <p className="mb-4">
+        To carry out something like this, try getting in touch with your local Neighborhood Advisory Committee, a nearby Community Development Corporation, or your local representative. Explain to them what you want to do and why. Show them this map to support your explanation!
+      </p>
 
-            Be mindful of trespassing on private property. You can use the dashboard tool above to identify which lots are privately owned. Consider reaching out to a lot owner and explaining why you want to carry out a lot cleanup in order to get permission.
+      <p className="mb-4">
+        Be mindful of trespassing on private property. You can use the dashboard tool above to identify which lots are privately owned. Consider reaching out to a lot owner and explaining why you want to carry out a lot cleanup in order to get permission.
+      </p>
 
-            Lastly, you can also use these data to convince your NAC, CDC, elected official, or another organization to take action. If you are interested in getting specific data that were used to build this tool, contact me and let me know how I can help.
-            Overview
-            Decision Tree
-            Potential Uses for Parcels
-            Things to Consider
-            Funding
-            Long-Term Maintenance
-            Gentrification
+      <p className="mb-4">
+        Lastly, you can also use these data to convince your NAC, CDC, elected official, or another organization to take action. If you are interested in getting specific data that were used to build this tool, contact me and let me know how I can help.
+      </p>
 
-      </div>
-      <div className="flex justify-between space-x-2.5">
+      <h1 className="text-3xl font-bold mb-4">Overview</h1>
+      <p>Content for the Overview section...</p>
 
-      </div>
+      <h2 className="text-2xl font-bold mt-4">Decision Tree</h2>
+      <p>Content for the Decision Tree section...</p>
 
+      <h2 className="text-2xl font-bold mt-4">Potential Uses for Parcels</h2>
+      <p>Content for the Potential Uses for Parcels section...</p>
+
+      <h1 className="text-3xl font-bold mt-4">Things to Consider</h1>
+      <p>Content for the Things to Consider section...</p>
+
+      <h2 className="text-2xl font-bold mt-4">Funding</h2>
+      <p>Content for the Funding section...</p>
+
+      <h2 className="text-2xl font-bold mt-4">Maintenance</h2>
+      <p>Content for the Maintenance section...</p>
+
+      <h2 className="text-2xl font-bold mt-4">Gentrification</h2>
+      <p>Content for the Gentrification section...</p>
+      
     </div>
+    <div className="flex justify-between space-x-2.5">
+      {/* Add buttons or other UI elements here */}
+    </div>
+  </div>
 );
 
 export default RecommendedActions;
