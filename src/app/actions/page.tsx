604d0749ddb6ddcc61b8eaf1d6788a2b37f75279
nlebovits <nissim.lebovits@vanderbilt.edu>
2023-09-27 15:24:36 -0400

    add basic actions page

---
+++ src/app/actions/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { FC } from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import Header from "../components/Header";
+import RecommendedActions from "../components/RecommendedActions";
+
+const Actions: FC = () => {
+  return (
+    <NextUIProvider>
+      <div className="h-screen overflow-hidden">
+        <Header />
+        <RecommendedActions />
+      </div>
+    </NextUIProvider>
+  );
+};
+
+export default Actions;
