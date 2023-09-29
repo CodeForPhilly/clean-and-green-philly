92dbe5f951873523eac6b7e8689837e2ce5492c8
nlebovits <nissim.lebovits@vanderbilt.edu>
2023-09-29 09:31:43 -0400

    add about page draft

--- src/app/components/AboutPage.tsx
+++ src/app/components/AboutPage.tsx
@@ -1,29 +1,60 @@
 import { Button, Link } from "@nextui-org/react";
 
 const AboutPage = () => (
-  <div className="container mx-auto pt-20 h-screen">
-    <div className="text-left text-2xl my-10">
-      <h1 className="text-3xl font-bold mb-4">Take Action</h1>
-      <p className="mb-4">What can I do with this?</p>
+  <div className="container mx-auto pt-20 h-screen grid grid-cols-1 sm:grid-cols-2 gap-8">
+    <div className="text-left my-10">
+      <h1 className="text-3xl font-bold mb-4">About</h1>
 
-      <ul className="list-disc pl-6 mb-6">
-        <li>Removing trash and debris</li>
-        <li>Grading the land</li>
-        <li>Planting a small number of trees to create a park-like setting</li>
-        <li>Installing low-perimeter (about 1 meter high) fences with multiple ungated entrances to encourage use</li>
-        <li>Regularly maintaining the lot</li>
-      </ul>
-
-      <p className="mb-4">
-        This approach has been shown to be inexpensive and cost-effective, with costs averaging about $5 per square meter and maintenance averaging $0.50 per square meter thereafter. If you’re not able to take on a full intervention, it’s still worthwhile to install a fence, plant trees, or even simply keep the lot free of trash, which can be done on your own.
+      <h2 className="text-2xl font-bold mt-4">Motivation</h2>
+      <p className="text-2xl mb-4">
+        Philadelphia has a gun violence problem. Homicides have been on the rise since 2013.
+        The past three years—2020, 2021, and 2022—have been the deadliest on record, with a high of 562 homicides in 2021.
+        Community members need solutions, but many city-run initiatives are frustratingly slow or inadequate.
+        Nearly 80% of the city’s anti-violence spending focuses on long-term violence reduction without any clear, immediate impact.
       </p>
 
-      <p className="mb-4">Where can I get help?</p>
-
-      <p className="mb-4">
-        To carry out something like this, try getting in touch with your local Neighborhood Advisory Committee, a nearby Community Development Corporation, or your local representative. Explain to them what you want to do and why. Show them this map to support your explanation!
+      <h2 className="text-2xl font-bold mt-4">Research</h2>
+      <p className="text-2xl mb-4">
+        Research shows that greening and cleaning vacant and abandoned parcels is one of the most impactful,
+        cost-effective interventions available to reduce gun violence in a neighborhood.
+        Drs. Eugenia South and Charles Branas have led several studies that demonstrate that greening vacant lots in Philadelphia reduced gun violence
+        by as much as 29% in the surrounding area. Similarly, cleaning and lightly repairing vacant houses led a 13% drop in gun assaults compared to nearby blocks.
+        These “greening and cleaning” interventions not only reduce gun violence but also provide other benefits, such as reducing the urban heat island effect,
+        lowering residents’ stress levels, and contributing to lower levels of depression among residents.
       </p>
-      
+
+      <h2 className="text-2xl font-bold mt-4">Methodology</h2>
+      <p className="text-2xl mb-4">
+        This map combines data on vacant and abandoned properties from the Department of Licenses and Inspections
+        with Philadelphia Police Department data on gun violence. Additional datasets from a variety of other sources
+        provide more information about each parcel, such as what neighborhood it falls in and whether it’s in the PHS Community LandCare
+        or Philadelphia LandCare programs.
+        <br></br>
+        The basic methodology is as follows. First, crime data were filtered to include only gun crimes (reported as “aggravated assault firearm”
+        and “robbery firearm” by PPD) in the past year. This follows the research upon which the dashboard is based. These points were then used to
+        calculate a <a href="https://pro.arcgis.com/en/pro-app/latest/tool-reference/spatial-analyst/how-kernel-density-works.htm" className="text-blue-500 hover:underline">kernel density estimate</a>,
+        which indicates the intensity of gun crime at any point in space. The KDE was then extracted to each vacant parcel to indicate the local level of gun violence.
+        Finally, to make the gun crime rates easier to understand, each parcel was classified by the percentile into which its gun crime score falls.
+        Those interested in a more in-depth understanding of how this map was produced can view the Python code behind the dataset in this GitHub repository.
+      </p>
+
+    </div>
+    <div className="text-left my-10">
+      <h2 className="text-2xl font-bold mt-4">Attribution</h2>
+      <p className="text-2xl mb-4">
+        This dashboard was built by Nissim Lebovits. He is a master's student in city planning at the University of Pennsylvania's Weitzman School of Design,
+        where he focuses on environmental planning and spatial analysis. He's interested in using data to foster civic engagement and build more sustainable,
+        inclusive cities. Previously he was an AmeriCorps VISTA with the City of Philadelphia, where he worked to support residents of the West Philadelphia Promise Zone.
+        For questions about this dashboard, to see more of his work, or to hire him to support your community organization's data work, please see his website.
+      </p>
+
+      <h2 className="text-2xl font-bold mt-4">Acknowledgements</h2>
+      <p className="text-2xl mb-4">
+        Many people helped inform this tool, including residents, CDC leaders, City staff, Penn faculty, and more. Special thanks, however,
+        are due to Dante Leonard for facilitating countless valuable conversations with people throughout Philadelphia, to Mjumbe Poe
+        for serving as my independent study instructor while I built the JavaScript dashboard and website, to Vicky Tam for teaching the GIS class
+        in which I built the first functional version of this tool, and to Dr. Eugenia South for spearheading the original research upon which this tool is based.
+      </p>
     </div>
     <div className="flex justify-between space-x-2.5">
       {/* Add buttons or other UI elements here */}
