891594836ddfcd7e2e088b0794899ae47ad0317e
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-10-08 10:53:11 -0400

    filters working

--- src/app/components/FilterView.tsx
+++ src/app/components/FilterView.tsx
@@ -1,18 +1,27 @@
-import React, { FC, useState } from "react";
-import DimensionFilter from "./filters/DimensionFilter";
+import React, { FC } from "react";
+import DimensionFilter from "./Filters/DimensionFilter";
+import MeasureFilter from "./Filters/MeasureFilter";
 
 const filters = [
   {
     property: "guncrime_density",
     display: "Gun Crime Density",
+    type: "dimension",
   },
   {
     property: "ZONINGBASEDISTRICT",
     display: "Zoning",
+    type: "dimension",
   },
   {
     property: "COUNCILDISTRICT_left",
     display: "Council District",
+    type: "dimension",
+  },
+  {
+    property: "tree_canopy_gap",
+    display: "Tree Canopy Gap",
+    type: "measure",
   },
 ];
 
@@ -21,9 +30,26 @@ const FilterView: FC = () => {
     <div className="p-6">
       <div className="font-semibold text-xl mb-2">Filters</div>
 
-      {filters.map(({ property, display }) => (
-        <DimensionFilter key={property} property={property} display={display} />
-      ))}
+      {filters.map(({ property, display, type }) => {
+        if (type === "dimension") {
+          return (
+            <DimensionFilter
+              key={property}
+              property={property}
+              display={display}
+            />
+          );
+        } else if (type === "measure") {
+          return (
+            <MeasureFilter
+              key={property}
+              property={property}
+              display={display}
+            />
+          );
+        }
+        return null;
+      })}
     </div>
   );
 };
