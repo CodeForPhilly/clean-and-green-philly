35b2579d23f58d9f91a6e5f344fe2ba217ef0ee2
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-11-14 19:53:35 -0500

    bc edits

--- src/app/components/PropertyMap.tsx
+++ src/app/components/PropertyMap.tsx
@@ -1,23 +1,20 @@
-import React, { FC, useEffect, useState, useRef } from "react";
-import {  Map as MapboxMap, VectorSource, SourceVectorLayer } from "mapbox-gl";
+import React, { useEffect, useState, useRef } from "react";
+import { Map as MapboxMap, PopupOptions } from "mapbox-gl";
 import { mapboxAccessToken, apiBaseUrl } from "../../config/config";
-import ZoomModal from "./ZoomModal";
 import { useFilter } from "@/context/FilterContext";
-import LegendControl from 'mapboxgl-legend';
-import 'mapboxgl-legend/dist/style.css';
-import '../globals.css';
-import Map, { 
-  Source, 
-  Layer, 
+import LegendControl from "mapboxgl-legend";
+import "mapboxgl-legend/dist/style.css";
+import "../globals.css";
+import Map, {
+  Source,
+  Layer,
   Popup,
-  NavigationControl, 
-  FullscreenControl, 
+  NavigationControl,
+  FullscreenControl,
   ScaleControl,
-  GeolocateControl
-} from 'react-map-gl';
-import type { FillLayer, CustomSource, VectorSourceRaw } from 'react-map-gl';
-
-//let popup: Popup | null = null;
+  GeolocateControl,
+} from "react-map-gl";
+import type { FillLayer, VectorSourceRaw } from "react-map-gl";
 
 const minZoom = 4;
 
@@ -26,8 +23,8 @@ const VectorTiles: VectorSourceRaw = {
   type: "vector",
   tiles: [`${apiBaseUrl}/api/generateTiles/{z}/{x}/{y}`],
   minzoom: minZoom,
-  maxzoom: 23
-}
+  maxzoom: 23,
+};
 
 const layerStyle: FillLayer = {
   id: "vacant_properties",
@@ -38,53 +35,41 @@ const layerStyle: FillLayer = {
     "fill-color": [
       "match",
       ["get", "guncrime_density"], // get the value of the guncrime_density property
-      "Bottom 50%", "#B0E57C", // Light Green
-      "Top 50%", "#FFD700", // Gold
-      "Top 25%", "#FF8C00", // Dark Orange
-      "Top 10%", "#FF4500", // Orange Red
-      "Top 5%", "#B22222", // FireBrick
-      "Top 1%", "#8B0000", // Dark Rednp
-      "#0000FF" // default color if none of the categories match
+      "Bottom 50%",
+      "#B0E57C", // Light Green
+      "Top 50%",
+      "#FFD700", // Gold
+      "Top 25%",
+      "#FF8C00", // Dark Orange
+      "Top 10%",
+      "#FF4500", // Orange Red
+      "Top 5%",
+      "#B22222", // FireBrick
+      "Top 1%",
+      "#8B0000", // Dark Rednp
+      "#0000FF", // default color if none of the categories match
     ],
-    "fill-opacity": 0.7
+    "fill-opacity": 0.7,
   },
   metadata: {
-    name: 'Guncrime Density',
-  }
+    name: "Guncrime Density",
+  },
 };
 
+const MapControls = () => (
+  <>
+    <GeolocateControl position="top-left" />
+    <FullscreenControl position="top-left" />
+    <NavigationControl position="top-left" />
+    <ScaleControl />
+  </>
+);
 
-
-function MapControls() {
-  return (
-    <>
-      <GeolocateControl position="top-left" />
-      <FullscreenControl position="top-left" />
-      <NavigationControl position="top-left" />
-      <ScaleControl />
-    </>
-  );
-}
-
-interface MapSourceAndLayerProps {
-  onClick: (event: MapEvent) => void;
-}
-
-const MapSourceAndLayer: React.FC<MapSourceAndLayerProps> = ({ onClick }) => {
-  return (
-    <Source id="vacant_properties" {...VectorTiles}>
-      <Layer {...layerStyle} onClick={onClick} />
-    </Source>
-  );
-};
-
-
-export default function PropertyMap() {
-  const [isZoomModalHidden, setIsZoomModalHidden] = useState(true);
+const PropertyMap = () => {
   const { filter } = useFilter();
   const [map, setMap] = useState<MapboxMap | null>(null);
-  const legend = new LegendControl();
-  const [popupInfo, setPopupInfo] = useState<any>(null);
+  const [popupInfo, setPopupInfo] = useState<any | null>(null);
+  const legendRef = useRef<LegendControl | null>(null);
 
   // filter function
   const updateFilter = () => {
@@ -125,39 +110,31 @@ export default function PropertyMap() {
 
   // handle map click
   const onMapClick = (event: any) => {
-    console.log(event); 
-
     if (map) {
-      const features = map.queryRenderedFeatures(event.point);
-      console.log(features);
+      const features = map.queryRenderedFeatures(event.point, {
+        layers: ["vacant_properties"],
+      });
 
       if (features.length > 0) {
-        setPopupInfo({ 
-          longitude: event.lngLat.lng, 
-          latitude: event.lngLat.lat, 
-          feature: features[0].properties
+        setPopupInfo({
+          longitude: event.lngLat.lng,
+          latitude: event.lngLat.lat,
+          feature: features[0].properties,
         });
+      } else {
+        setPopupInfo(null);
       }
     }
   };
-
-  // handle zoom
-  const onMapZoomEnd = () => {
-    if (map) {
-      setIsZoomModalHidden(map.getZoom() >= minZoom);
-    }
-  };
-
-  const legendRef = useRef<LegendControl | null>(null);
 
   useEffect(() => {
     if (map) {
       if (!legendRef.current) {
         legendRef.current = new LegendControl();
-        map.addControl(legendRef.current, 'bottom-left');
+        map.addControl(legendRef.current, "bottom-left");
       }
     }
-  
+
     return () => {
       if (map && legendRef.current) {
         map.removeControl(legendRef.current);
@@ -165,7 +142,7 @@ export default function PropertyMap() {
       }
     };
   }, [map]);
-  
+
   // Filter update
   useEffect(() => {
     if (map) {
@@ -173,202 +150,50 @@ export default function PropertyMap() {
     }
   }, [map, filter, updateFilter]);
 
+  const changeCursor = (e: any, cursorType: "pointer" | "default") => {
+    e.target.getCanvas().style.cursor = cursorType;
+  };
+
   // map load
   return (
     <div className="relative h-full w-full">
-    <Map
-      mapboxAccessToken={mapboxAccessToken}
-      mapLib={import('mapbox-gl')}
-      initialViewState={{
-        longitude: -75.1652,
-        latitude: 39.9526,
-        zoom: 13,
-      }}
-      mapStyle="mapbox://styles/mapbox/light-v10"
-      onClick={onMapClick}
-      onZoomEnd={onMapZoomEnd}
-      interactiveLayerIds={["vacant_properties"]}
-      onLoad={(e) => {
-        setMap(e.target);
-      }}
-    >
-      <MapControls />
-      
-      {popupInfo && (
-        <Popup
-          longitude={popupInfo.longitude}
-          latitude={popupInfo.latitude}
-          onClose={() => setPopupInfo(null)}
-        >
-          <div>
-            <p className="font-bold">{popupInfo.feature.ADDRESS}</p>
-            <p>Owner: {popupInfo.feature.OWNER1}</p>
-            <p>Gun Crime Density: {popupInfo.feature.guncrime_density}</p>
-            <p>Tree Canopy Gap: {popupInfo.feature.tree_canopy_gap}</p>
-          </div>
-        </Popup>
-        
-      )}
-      <MapSourceAndLayer onClick={onMapClick} />
-    </Map>
-    </div>
-  );
-}
+      <Map
+        mapboxAccessToken={mapboxAccessToken}
+        initialViewState={{
+          longitude: -75.1652,
+          latitude: 39.9526,
+          zoom: 13,
+        }}
+        mapStyle="mapbox://styles/mapbox/light-v10"
+        onMouseEnter={(e) => changeCursor(e, "pointer")}
+        onMouseLeave={(e) => changeCursor(e, "default")}
+        onClick={onMapClick}
+        interactiveLayerIds={["vacant_properties"]}
+        onLoad={(e) => {
+          setMap(e.target);
+        }}
+      >
+        <MapControls />
 
-/*
-interface PropertyMapProps {
-  setFeaturesInView: (features: any[]) => void;
-}
-
-const PropertyMap: FC<PropertyMapProps> = ({ setFeaturesInView }) => {
-  const [isZoomModalHidden, setIsZoomModalHidden] = useState(true);
-  const { filter } = useFilter();
-  const [map, setMap] = useState<MapboxMap | null>(null);
-
-  const updateFilter = () => {
-    if (!map) return;
-
-    const isAnyFilterEmpty = Object.values(filter).some((filterItem) => {
-      if (filterItem.type === "dimension") {
-        return filterItem.values.length === 0;
-      } else if (filterItem.type === "measure") {
-        return filterItem.min === filterItem.max;
-      }
-      return true;
-    });
-
-    if (isAnyFilterEmpty) {
-      map.setFilter("vacant_properties", ["==", ["id"], ""]);
-      return;
-    }
-
-    const mapFilter = Object.entries(filter).reduce(
-      (acc, [property, filterItem]) => {
-        if (filterItem.type === "dimension" && filterItem.values.length) {
-          acc.push(["in", property, ...filterItem.values]);
-        } else if (
-          filterItem.type === "measure" &&
-          filterItem.min !== filterItem.max
-        ) {
-          acc.push([">=", property, filterItem.min]);
-          acc.push(["<=", property, filterItem.max]);
-        }
-        return acc;
-      },
-      [] as any[]
-    );
-
-    map.setFilter("vacant_properties", ["all", ...mapFilter]);
-  };
-
-  useEffect(() => {
-    const mapInstance = new MapboxMap({
-      container: "mapContainer",
-      style: "mapbox://styles/mapbox/light-v10",
-      center: [-75.1652, 39.9526],
-      zoom: 13,
-      accessToken: mapboxAccessToken,
-    });
-
-    mapInstance.on("load", async () => {
-      const minZoom = 4;
-
-      mapInstance.on("zoomend", () => {
-        setIsZoomModalHidden(mapInstance.getZoom() >= minZoom);
-      });
-
-      mapInstance.addControl(new NavigationControl(), 'top-left');
-      mapInstance.addControl(new FullscreenControl(), 'top-left');
-
-      mapInstance.addSource("vacant_properties", {
-        type: "vector",
-        tiles: [`${apiBaseUrl}/api/generateTiles/{z}/{x}/{y}`],
-        minzoom: minZoom,
-        maxzoom: 23,
-      });
-
-      mapInstance.addLayer({
-        id: "vacant_properties",
-        type: "fill",
-        source: "vacant_properties",
-        "source-layer": "vacant_properties",
-        paint: {
-          "fill-color": [
-            "match",
-            ["get", "guncrime_density"], // get the value of the guncrime_density property
-            "Bottom 50%", "#B0E57C", // Light Green
-            "Top 50%", "#FFD700", // Gold
-            "Top 25%", "#FF8C00", // Dark Orange
-            "Top 10%", "#FF4500", // Orange Red
-            "Top 5%", "#B22222", // FireBrick
-            "Top 1%", "#8B0000", // Dark Rednp
-            "#0000FF" // default color if none of the categories match
-          ],
-          "fill-opacity": 0.7
-        },
-        metadata: {
-          name: 'Guncrime Density',
-        }
-      });
-      
-      updateFilter();
-
-      const legend = new LegendControl();
-      mapInstance.addControl(legend, 'bottom-left');
-
-      mapInstance.on("moveend", () => {
-        let features = mapInstance.queryRenderedFeatures();
-        features = features.filter(
-          (feature) => feature.layer.id === "vacant_properties"
-        );
-        setFeaturesInView(features);
-      });
-
-      mapInstance.on("click", "vacant_properties", (e) => {
-        if (popup) {
-          popup.remove();
-        }
-
-        if (e.features?.length) {
-          const feature = e.features[0];
-          const propertyHTML = feature.properties
-            ? Object.keys(feature.properties)
-                .map((key) => `<b>${key}</b>: ${feature.properties![key]}<br/>`)
-                .join("")
-            : "No properties available";
-
-          popup = new Popup({ offset: [0, -15] })
-            .setLngLat(e.lngLat)
-            .setHTML(`<div style='color: black;'><p>${propertyHTML}</p></div>`)
-            .addTo(mapInstance);
-        }
-      });
-
-      mapInstance.on("mouseenter", "vacant_properties", () => {
-        mapInstance.getCanvas().style.cursor = "pointer";
-      });
-
-      mapInstance.on("mouseleave", "vacant_properties", () => {
-        mapInstance.getCanvas().style.cursor = "";
-      });
-
-      setMap(mapInstance);
-    });
-  }, []);
-
-  useEffect(() => {
-    updateFilter();
-  }, [filter]);
-
-  return (
-    <div className="relative h-full w-full">
-      <div id="mapContainer" className="h-full w-full"></div>
-      <ZoomModal isHidden={isZoomModalHidden} />
+        {popupInfo && (
+          <Popup
+            longitude={popupInfo.longitude}
+            latitude={popupInfo.latitude}
+            closeOnClick={false}
+          >
+            <div>
+              <p className="font-bold">{popupInfo.feature.ADDRESS}</p>
+              <p>Owner: {popupInfo.feature.OWNER1}</p>
+              <p>Gun Crime Density: {popupInfo.feature.guncrime_density}</p>
+              <p>Tree Canopy Gap: {popupInfo.feature.tree_canopy_gap}</p>
+            </div>
+          </Popup>
+        )}
+        <Source id="vacant_properties" {...VectorTiles}>
+          <Layer {...layerStyle} />
+        </Source>
+      </Map>
     </div>
   );
 };
-
-
 export default PropertyMap;
-
-*/
