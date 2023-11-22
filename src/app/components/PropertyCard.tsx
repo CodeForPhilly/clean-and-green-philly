b1352cadd952281445cb6924a3086f862af09a7d
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-11-21 23:04:32 -0500

    checkin

--- src/app/components/PropertyCard.tsx
+++ src/app/components/PropertyCard.tsx
@@ -1,38 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { Chip } from "@nextui-org/react";
+import PropertyDetail from "./PropertyDetail";
 
 interface PropertyCardProps {
   feature: any;
 }
 
 const PropertyCard = ({ feature }: PropertyCardProps) => {
+  const [showDetail, setShowDetail] = useState(false);
+
   const { ADDRESS, guncrime_density, tree_canopy_gap } = feature.properties;
 
   const roundedCanopyGap = Math.round(tree_canopy_gap * 100);
   const randomImage = `/image${Math.floor(Math.random() * 3) + 1}.jpg`;
 
+  const toggleDetail = () => setShowDetail(!showDetail);
+
+  if (showDetail) {
+    return <PropertyDetail feature={feature} />;
+  }
+
   return (
-    <div className="max-w-sm w-full md:w-1/2 p-4">
-      <div className="bg-white rounded-lg overflow-hidden">
-        <div className="relative h-48 w-full rounded-lg overflow-hidden">
-          <Image
-            src={randomImage}
-            alt={`Property at ${ADDRESS}`}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <div className="p-4">
-          <div className="font-bold text-xl">{ADDRESS}</div>
-          <div className="text-gray-700 mb">
-            {guncrime_density} Crime Rate, {roundedCanopyGap}% Canopy Gap
+    <div
+      className="max-w-sm w-full md:w-1/2 p-4 cursor-pointer"
+      onClick={toggleDetail}
+    >
+      <div className="max-w-sm w-full md:w-1/2 p-4">
+        <div className="bg-white rounded-lg overflow-hidden">
+          <div className="relative h-48 w-full rounded-lg overflow-hidden">
+            <Image
+              src={randomImage}
+              alt={`Property at ${ADDRESS}`}
+              layout="fill"
+              objectFit="cover"
+            />
           </div>
-        </div>
-        <div className="px-4 pb-2">
-          <Chip color="success" variant="solid" size="sm">
-            High Priority
-          </Chip>
+          <div className="p-4">
+            <div className="font-bold text-xl">{ADDRESS}</div>
+            <div className="text-gray-700 mb">
+              {guncrime_density} Crime Rate, {roundedCanopyGap}% Canopy Gap
+            </div>
+          </div>
+          <div className="px-4 pb-2">
+            <Chip color="success" variant="solid" size="sm">
+              High Priority
+            </Chip>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/PropertyDetail.tsx
+++ src/app/components/PropertyDetail.tsx
@@ -1,1 +1,28 @@
 import React from "react";
+
+const PropertyDetail = () => {
+  return (
+    <div className="max-w-sm w-full md:w-1/2 p-4">
+      <div className="bg-white rounded-lg overflow-hidden">
+        <div className="relative h-48 w-full rounded-lg overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1612839905599-2b3f8f3e3d2a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvcGVydGllcyUyMHRvJTIwYmFjayUyMHN0YXRpb25zJTIwYW5kJTIwY2FuYXBpJTIwZ2FtZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
+            alt="Property at 123 Main St"
+            className="absolute h-full w-full object-cover"
+          />
+        </div>
+        <div className="p-4">
+          <div className="font-bold text-xl">123 Main St</div>
+          <div className="text-gray-700 mb">0.5 Crime Rate, 0% Canopy Gap</div>
+        </div>
+        <div className="px-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            High Priority
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyDetail;
