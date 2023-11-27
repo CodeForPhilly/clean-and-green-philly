4748434bbff9f7995e1ab810b2782eb8026455f5
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-11-26 21:34:46 -0500

    checking and clean up branch

--- src/app/components/PropertyCard.tsx
+++ src/app/components/PropertyCard.tsx
@@ -4,35 +4,41 @@ import { Chip } from "@nextui-org/react";
 
 interface PropertyCardProps {
   feature: any;
+  setSelectedProperty: (feature: any) => void;
 }
 
-const PropertyCard = ({ feature }: any) => {
+const PropertyCard = ({ feature, setSelectedProperty }: PropertyCardProps) => {
   const { ADDRESS, guncrime_density, tree_canopy_gap } = feature.properties;
 
   const roundedCanopyGap = Math.round(tree_canopy_gap * 100);
-  const randomImage = `/image${Math.floor(Math.random() * 3) + 1}.jpg`;
+  const randomImage = `/image1.jpg`;
 
   return (
-    <div className="max-w-sm w-full md:w-1/2 p-4">
-      <div className="bg-white rounded-lg overflow-hidden">
-        <div className="relative h-48 w-full rounded-lg overflow-hidden">
-          <Image
-            src={randomImage}
-            alt={`Property at ${ADDRESS}`}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <div className="p-4">
-          <div className="font-bold text-xl">{ADDRESS}</div>
-          <div className="text-gray-700 mb">
-            {guncrime_density} Crime Rate, {roundedCanopyGap}% Canopy Gap
+    <div
+      className="max-w-sm w-full md:w-1/2 p-4 cursor-pointer"
+      onClick={() => setSelectedProperty(feature)}
+    >
+      <div className="max-w-sm w-full p-4">
+        <div className="bg-white rounded-lg overflow-hidden">
+          <div className="relative h-48 w-full rounded-lg overflow-hidden">
+            <Image
+              src={randomImage}
+              alt={`Property at ${ADDRESS}`}
+              layout="fill"
+              objectFit="cover"
+            />
           </div>
-        </div>
-        <div className="px-4 pb-2">
-          <Chip color="success" variant="solid" size="sm">
-            High Priority
-          </Chip>
+          <div className="p-4">
+            <div className="font-bold text-xl">{ADDRESS}</div>
+            <div className="text-gray-700 mb">
+              {guncrime_density} Crime Rate, {roundedCanopyGap}% Canopy Gap
+            </div>
+          </div>
+          <div className="px-4 pb-2">
+            <Chip color="success" variant="solid" size="sm">
+              High Priority
+            </Chip>
+          </div>
         </div>
       </div>
     </div>
