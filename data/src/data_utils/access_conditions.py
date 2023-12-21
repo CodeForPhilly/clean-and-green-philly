4eb72fd8599adb85cf6bf572b7be935bd0a72c57
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2023-12-21 13:50:00 -0500

    add notes for access conditions

---
+++ data/src/data_utils/access_conditions.py
@@ -0,0 +1,9 @@
+### conditions needed for property access:
+
+# ownership--city or private
+# sold within last six months
+# facing foreclosure
+# has l&i violations for vacant, unsafe, and notorious
+# has tax debt or delinquency?
+# is it land or a building?
+# what does it cost?
