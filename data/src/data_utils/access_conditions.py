9362a7629df0a85e285d074decec9f318237c527
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2023-12-21 14:17:47 -0500

    add more notes on conditions

--- data/src/data_utils/access_conditions.py
+++ data/src/data_utils/access_conditions.py
@@ -1,9 +1,25 @@
 ### conditions needed for property access:
 
 # ownership--city or private
+# if "city_agency_owner" == "PLB" ~ "land bank"
+# elif "city_agency_owner" == NA ~ "private owner"
+# else "city, but too complicated"
+    
 # sold within last six months
-# facing foreclosure
+# sale_date <= 6 months ago
+
+# facing foreclosure (e.g., is the foreclosure column a Y)
+# if "sheriff_sale" == "Y"
+
 # has l&i violations for vacant, unsafe, and notorious
+
 # has tax debt or delinquency?
+# total_due != NA
+
 # is it land or a building?
+# if "parcel_type" == "Lot"
+# else "parcel type" == "Building"
+
 # what does it cost?
+# if "market_value" < 1000 ~ "buy property"
+# else "private land use agreement"
