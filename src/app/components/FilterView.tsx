8c001e9a3cacd088703c34a630fbe7e160ea8a45
Brandon Cohen <brandon.f.cohen@gmail.com>
2024-01-11 09:33:21 -0500

    filters working

--- src/app/components/FilterView.tsx
+++ src/app/components/FilterView.tsx
@@ -1,32 +1,26 @@
 import React, { FC } from "react";
 import DimensionFilter from "./Filters/DimensionFilter";
-import MeasureFilter from "./Filters/MeasureFilter";
 
 const filters = [
   {
-    property: "guncrime_density",
-    display: "Gun Crime Density",
-    type: "dimension",
+    property: "priority_level",
+    display: "Priority Level",
+    options: ["Low Priority", "Medium Priority", "High Priority"],
   },
   {
-    property: "all_violations_past_year",
-    display: "L&I Violations in the Past Year",
-    type: "measure",
+    property: "parcel_type",
+    display: "Parcel Type",
+    options: ["Land", "Building"],
   },
   {
-    property: "ZONINGBASEDISTRICT",
-    display: "Zoning",
-    type: "dimension",
-  },
-  {
-    property: "COUNCILDISTRICT_left",
-    display: "Council District",
-    type: "dimension",
-  },
-  {
-    property: "tree_canopy_gap",
-    display: "Tree Canopy Gap",
-    type: "measure",
+    property: "access_process",
+    display: "Access Process",
+    options: [
+      "Buy Property",
+      "Land Bank",
+      "Private Land Use Agreement",
+      "Conservatorship",
+    ],
   },
 ];
 
@@ -35,26 +29,14 @@ const FilterView: FC = () => {
     <div className="p-6">
       <div className="font-semibold text-xl mb-2">Filters</div>
 
-      {filters.map(({ property, display, type }) => {
-        if (type === "dimension") {
-          return (
-            <DimensionFilter
-              key={property}
-              property={property}
-              display={display}
-            />
-          );
-        } else if (type === "measure") {
-          return (
-            <MeasureFilter
-              key={property}
-              property={property}
-              display={display}
-            />
-          );
-        }
-        return null;
-      })}
+      {filters.map(({ property, display, options }) => (
+        <DimensionFilter
+          key={property}
+          property={property}
+          options={options}
+          display={display}
+        />
+      ))}
     </div>
   );
 };
