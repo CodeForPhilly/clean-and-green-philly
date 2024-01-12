cd0a2853526e5793013e53c91b5228fa48d62066
Brandon Cohen <brandon.f.cohen@gmail.com>
2024-01-11 21:33:30 -0500

    revert psql changes

--- data/src/config/psql.py
+++ data/src/config/psql.py
@@ -2,7 +2,6 @@ import os
 from sqlalchemy import create_engine
 
 local_engine = create_engine(
-    # .replace("localhost", "host.docker.internal")
-    os.environ["VACANT_LOTS_DB"]
+    os.environ["VACANT_LOTS_DB"].replace("localhost", "host.docker.internal")
 )
 conn = local_engine.connect()
