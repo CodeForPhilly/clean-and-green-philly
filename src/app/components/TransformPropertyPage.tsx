b8bbd97fd54d465ecac5adbe2740d8e218f4bcd6
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-01-14 11:55:47 -0500

    update draft of transform a property page (minimal styling)

--- src/app/components/TransformPropertyPage.tsx
+++ src/app/components/TransformPropertyPage.tsx
@@ -7,26 +7,83 @@ export default function TransformPropertyPage() {
     <div className="container mx-auto pt-20 h-screen">
     <h1 className="text-4xl font-bold mb-6">Transform a Property</h1>
 
-    <h2 className="text-2xl font-bold mb-4">What can I do?</h2>
-    <p>This project focuses on "cleaning and greening" as ways to help reduce gun violence. This includes:</p>
+
+    <p>
+      Clean & Green Philly aims to help reduce gun violence by intervening in vacant properties. Very simply, this means improving the property. This can range from removing trash to planting a community garden, from trimming trees to building affordable housing. Below, we lay out ways to transform a vacant property into something that positively impacts the neighborhood around it.
+    </p>
+
+    <h2 className="text-2xl font-bold mt-8">Types of Interventions</h2>
+    <p>
+    The original research that Clean & Green Philly is based on promotes “cleaning and greening”. This includes removing trash, grading the land, planting trees, installing low fences, and maintaining the property. These are low-cost interventions and have been proven to be highly effective; even something as simple as cleaning up trash can have a big impact. 
+
+    That said, there are many ways to transform a vacant property. You could, for example:
+
     <ul className="list-disc pl-6 mb-4">
-        <li>Removing trash and debris: Cleaning up properties helps make them safe and nice to look at.</li>
-        <li>Grading the land: This helps manage water and can stop flooding, making the property better for use.</li>
-        <li>Planting trees: A few trees can turn an empty space into a small park, offering shade and a place to relax.</li>
-        <li>Installing low fences: Fences about 1 meter high can help keep the space neat and welcoming without closing it off.</li>
-        <li>Regular maintenance: This is important to keep the space clean, safe, and nice over time.</li>
+      <li>Partner with a local restaurant to offer outdoor eating</li>
+      <li>Install bike parking</li>
+      <li>Work with the Philadelphia Water Department to install green stormwater infrastructure</li>
+      <li>Plant a pollinator garden to attract birds, butterflies, and bees</li>
+      <li>Connect with a non-profit organization to build affordable housing</li>
     </ul>
-    <p>The cost for these kinds of projects is usually low, about $5 per square yard, with maintenance around $0.50 per square yard. Even if you can't do everything, small steps like putting up a fence, planting some trees, or just keeping the area clean can make a big difference.</p>
+    These are just some of the many possibilities. Remember to be both creative and practical: there is no one-size-fits-all solution, and Philadelphia will need a variety of uses in its vacant properties, including green space, housing, parks, commercial space, and more.
+    </p>
 
-    <h3 className="text-xl font-bold mt-8 mb-4">Figure out what's right for your property</h3>
-    <p>Every property is different. The <a href="https://detroitfuturecity.com/whatwedo/land-use/DFC-lots/" className="text-blue-500 hover:underline">Detroit Future City lot quiz</a> is a good way to start thinking about what could work for your space. Look at things like how much sun the property gets, what kind of soil it has, and what plants are already there. This will help you decide the best way to use the property.</p>
+    <h2 className="text-2xl font-bold mt-8">What's right for my property?</h2>
+    <p>
+      Different properties have different opportunities and different challenges. Some properties might be a great spot for a rain garden but not for a playground. Likewise, you wouldn’t want to start a community garden in a vacant lot that doesn’t get any sun. To better understand what your options are for a specific property, try filling out the Detroit Future City vacant property quiz [https://detroitfuturecity.com/whatwedo/land-use/DFC-lots/]. It’s meant to help users understand the best options for their specific properties. Although it was built for users in Detroit, it’s still helpful here in Philadelphia. Also, remember to think about other constraints, like how much funding you have, and how much maintenance you’re willing or able to do.
+    </p>
 
-    <h3 className="text-xl font-bold mt-8 mb-4">Remember, there's no one right way</h3>
-    <p>Every part of Philadelphia is different and needs different things. It's important to pick a plan that fits your area. Talk to the people who live and work near you, and the leaders in your community. You might find that some places need spaces for families, while others might do well with a garden or places to live that people can afford. It's important to think about how to help people stay in their neighborhoods while also making sure there's enough housing for everyone.</p>
+    <h2 className="text-2xl font-bold mt-8">Where can I get help?</h2>
+    <p>
+    Many organizations in Philadelphia offer support transforming vacant properties.
+    </p>
+    <h3 className="text-xl font-bold mt-8 mb-4">Community Groups and Local Representatives</h3>
+    <p>For help with greening projects, reach out to local Neighborhood Advisory Committees, Community Development Corporations, or Registered Community Organizations. They can guide you through the planning process and help with local rules. Local representatives can also support you, especially if you're facing red tape. Bring them a solid plan and show how your project can improve the community.</p>
 
-    <h3 className="text-xl font-bold mt-8 mb-4">Be creative</h3>
-    <p>Think about other ways you can use empty properties. Besides making parks or gardens, there are lots of options. You could turn a property into a place for outdoor music, a project to help manage rainwater, a place for birds, or an outdoor art space. There are many possibilities, so use your imagination and think about what your community needs most. By being creative, you can help turn these spaces into important and loved parts of your neighborhood.</p>
+    <h3 className="text-xl font-bold mt-8 mb-4">Garden Justice Legal Initiative</h3>
+    <p>The <a href="https://pubintlaw.org/cases-and-projects/garden-justice-legal-initiative-gjli/" className="text-blue-500 hover:underline">Garden Justice Legal Initiative</a> supports community gardens and urban farms, offering free legal help, policy research, and community education. They work with coalitions like <a href="https://soilgeneration.org/" className="text-blue-500 hover:underline">Soil Generation</a> to empower residents to use vacant land for gardens and farming, providing tools and information through training sessions and their online resource, <a href="https://groundedinphilly.org/" className="text-blue-500 hover:underline">Grounded in Philly</a>.</p>
 
-</div>
+    <h3 className="text-xl font-bold mt-8 mb-4">StreetBoxPHL</h3>
+    <p><a href="https://streetboxphl.com/" className="text-blue-500 hover:underline">StreetBoxPHL</a> helps community groups revitalize urban spaces by making parklets, pedestrian plazas, and bike corrals. Their tools can quickly transform underused spaces into vibrant, green areas that benefit the whole community. They offer technical support, a street furniture library, and help with insurance requirements.</p>
+
+    <h3 className="text-xl font-bold mt-8 mb-4">Park in a Truck</h3>
+    <p><a href="https://www.jefferson.edu/academics/colleges-schools-institutes/architecture-and-the-built-environment/programs/landscape-architecture/park-in-a-truck.html" className="text-blue-500 hover:underline">Park in a Truck</a> supports community residents in quickly building custom parks. They've developed <a href="https://www.jefferson.edu/content/dam/academic/cabe/landscape-architecture/park-in-a-truck/toolkit/PiaT-toolkit-2022.pdf" className="text-blue-500 hover:underline">a toolkit</a> that helps community groups design and build parks that fit their needs. They also offer training and technical support to help communities create and maintain their parks.</p>
+    
+    <h2 className="text-2xl font-bold mt-8">What else should I think about?</h2>
+
+    <p>
+
+    <Accordion variant="light" selectionMode="multiple">
+
+    <AccordionItem key="1" aria-label="Maintenance" title="Maintenance" subtitle="Click to expand" className="font-bold text-large">
+    <div className="text-base font-normal">
+    It's also key to make sure that the improvements you make can keep going for a long time. Getting people in the area to care about and look after these spaces can really help. Choose projects that don't cost much but still have a big impact, so they're easier for the community to maintain. Be smart about legal stuff, especially with private property, and work with local authorities to make sure everything is okay there.
+    </div>
+    </AccordionItem>
+
+    <AccordionItem key="2" aria-label="Funding" title="Funding" subtitle="Click to expand" className="font-bold text-large">
+    <div className="text-base font-normal">
+    When it comes to funding, try to connect with local businesses and community groups. Small grants and donations might be easier to get for groups like yours. And don't forget about raising money right in the community through events or online campaigns.
+    </div>
+    </AccordionItem>
+
+    <AccordionItem key="3" aria-label="Gentrification" title="Gentrification" subtitle="Click to expand" className="font-bold text-large">
+    <div className="text-base font-normal">
+    Remember, your projects shouldn't push out the current residents. Keep them at the heart of what you do, and keep an eye on how things change in the neighborhood. Work together with other local groups, schools, and businesses - it's a great way to get more resources and ideas. Have regular meetings with people in the community to keep them in the loop and involved in decisions.
+    </div>
+    </AccordionItem>
+
+    </Accordion>
+
+    </p>
+
+    <h2 className="text-2xl font-bold mt-8">Disclaimer</h2>
+    <p>
+        This website is not legal advice. It is intended to provide general information in summary form on legal topics, current at the time of first publication. The contents do not constitute legal advice and should not be relied upon as such. Formal legal advice should be sought in particular matters.
+        <br /><br />
+        Clean & Green Philly is not responsible for the content of any third-party sites linked to from this website. Links are provided as a convenience only, and do not imply endorsement of the linked site by Clean & Green Philly.
+    </p>
+
+    </div>
   );
 }
