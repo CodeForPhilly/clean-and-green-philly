b201abc4f8daef779e3e6ac6294d5eef9817f1a8
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2024-01-18 12:19:16 -0500

    update language

--- src/app/components/RequestRemovalPage.tsx
+++ src/app/components/RequestRemovalPage.tsx
@@ -14,7 +14,7 @@ export default function GetAccessPage() {
     In order to do this, please email us at <a href="mailto:cleanandgreenphl@gmail.com" className="text-primary">cleangreenphilly@gmail.com</a>. Submit a detailed request for removal, including the specific address of the property and a thorough explanation of your reasons for requesting its removal. Documentation such as photos of the site in use are encouraged. We will review the reason for your request and respond as quickly as we can. In making our decision, we will consider whether we believe the request is reasonable and consistent with positive community impact and anti-gun violence initiatives.
     <br></br>
     <br></br>
-    Our team reserves the right to make the final decision on whether a property will be removed from the dashboard. Please note that requests to remove properties solely for personal gain, such as protecting private parking spaces or assisting negligent developers, will not be entertained. By submitting a request, you acknowledge and agree that the final decision rests with our team, and not all requests may result in the removal of a property listing.
+    Our team reserves the right to make the final decision on whether a property will be removed from the dashboard. Please note that requests to remove properties solely for personal gain, such as protecting private parking spaces or assisting negligent property owners, will not be entertained. By submitting a request, you acknowledge and agree that the final decision rests with our team, and not all requests may result in the removal of a property listing.
     </p>
 
     </div>
