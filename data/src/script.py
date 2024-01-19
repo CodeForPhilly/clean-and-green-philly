8767561f529b539c761ff9d5192e2466e4f0f007
Brandon Cohen <brandon.f.cohen@gmail.com>
2024-01-18 22:29:43 -0500

    update tactical urbanism

--- data/src/script.py
+++ data/src/script.py
@@ -15,19 +15,19 @@ if awkde_path not in sys.path:
     sys.path.append(awkde_path)
 
 services = [
-   city_owned_properties,
-   phs_properties,
-   l_and_i,
-   rco_geoms,
-   tree_canopy,
-   nbhoods,
-   gun_crimes,
-   drug_crimes,
-   deliquencies,
-   opa_properties,
-   unsafe_buildings,
-   imm_dang_buildings,
-   tactical_urbanism,
+    city_owned_properties,
+    phs_properties,
+    l_and_i,
+    rco_geoms,
+    tree_canopy,
+    nbhoods,
+    gun_crimes,
+    drug_crimes,
+    deliquencies,
+    opa_properties,
+    unsafe_buildings,
+    imm_dang_buildings,
+    tactical_urbanism,
 ]
 
 
@@ -46,7 +46,7 @@ dataset = access_process(dataset)
 
 
 # Post to Mapbox
-# dataset.upload_to_mapbox("vacant_properties")
+dataset.upload_to_mapbox("vacant_properties")
 
 dataset.gdf.to_postgis(
     "vacant_properties_end", conn, if_exists="replace", index=False
