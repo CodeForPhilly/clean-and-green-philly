d133f5ffb1c264395a5354fb420455c9d7b245e8
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-01-24 19:08:30 -0500

    fix legal disclaimer page

--- src/app/components/LegalDisclaimerPage.tsx
+++ src/app/components/LegalDisclaimerPage.tsx
@@ -10,44 +10,111 @@ import { get } from "http";
 export default function LegalDisclaimerPage() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-grow container mx-auto pt-20">
-        <h1 className="text-4xl font-bold mb-6">Overview</h1>
+      <div className="container mx-auto pt-20">
+        <h1 className="text-4xl font-bold mb-6">Legal Disclaimer</h1>
 
+        <h2 className="text-2xl font-bold mt-8">
+          Acceptance of Terms and Conditions of Use
+        </h2>
         <p>
-          Clean & Green Philly can help you prioritize vacant properties to return to productive use. We encourage cleaning and greening interventions, but also recognize that there is no one-size-fits-all solution. To address gun violence and vacancy in Philadelphia, we will need a range of interventions, each tailored to local context. Below, we lay out ways that you can use this site to identify a property, understand how to get legal access to it, and think about the ways in which you might best return it to productive use—whether through cleaning and greening or otherwise.
+          By using this website, you accept and agree to these Terms and
+          Conditions of Use. Clean & Green Philly reserves the right to change,
+          modify, add, or remove portions of these terms and conditions at its
+          discretion at any time. Please check these Terms and Conditions of Use
+          periodically for changes. Your continued use of this website following
+          the posting of changes to the Terms and Conditions of Use will mean
+          that you accept the changes.
         </p>
 
-        <h2 className="text-2xl font-bold mt-8">What can I do?</h2>
+        <h2 className="text-2xl font-bold mt-8">
+          Information Not Legal Advice
+        </h2>
         <p>
-          Using this site, you can identify a high impact property, figure out how to get legal access to it, and decide on your preferred intervention to return the lot to productive use. 
+          This website provides information to help Philadelphia residents
+          identify priority vacant properties for anti-gun violence
+          interventions. The information on this website is not legal advice.
+          Legal information is not the same as legal advice, which is the
+          application of law to an individual’s specific circumstances. The
+          information on this website is not a substitute for and does not
+          replace the advice or representation of a licensed attorney. Although
+          Clean & Green Philly goes to great lengths to make sure the
+          information on the website is accurate and up to date, we make no
+          claim as to the accuracy of this information and are not responsible
+          for any consequences that may result from the use of this website. We
+          recommend that you consult with a licensed attorney if you want
+          assurance that the information on the website and your interpretation
+          of it are appropriate for your particular situation. You should not
+          and are not authorized to rely on this website as a source of legal
+          advice. The use of this website does not create an attorney-client
+          relationship between Clean & Green Philly and any user.
         </p>
 
-        <p>At a minimum, we encourage cleaning and greening. This involves:</p>
-        <ul className="list-disc pl-6 mb-4">
-          <li>Removing trash and debris</li>
-          <li>Grading the land</li>
-          <li>Planting trees</li>
-          <li>Installing low fences</li>
-          <li>Regular maintenance</li>
-        </ul>
-
+        <h2 className="text-2xl font-bold mt-8">Errors or Omissions</h2>
         <p>
-          The cost for these kinds of projects is usually low, about $5 per square yard, with maintenance around $0.50 per square yard. Even if you can't do everything, small steps like putting up a fence, planting some trees, or just keeping the area clean can make a big difference.
+          While we have made every attempt to ensure that the information
+          contained in this website has been obtained from reliable sources,
+          Clean & Green Philly is not responsible for any errors or omissions,
+          or for the results obtained from the use of this information. All
+          information in this website is provided "as is", with no guarantee of
+          completeness, accuracy, timeliness or of the results obtained from the
+          use of this information, and without warranty of any kind, express or
+          implied, including, but not limited to warranties of performance,
+          merchantability and fitness for a particular purpose. In no event will
+          Clean & Green Philly, its related partnerships or corporations, or the
+          partners, agents or employees thereof be liable to you or anyone else
+          for any decision made or action taken in reliance on the information
+          in this website or for any consequential, special or similar damages,
+          even if advised of the possibility of such damages.
         </p>
 
-        <h2 className="text-2xl font-bold mt-8">How can Clean & Green Philly help me take action?</h2>
-
-        <p>Basic use of Clean & Green Philly is simple:</p>
-        <ol className="list-decimal pl-6 mb-4">
-          <li>Identify a high-impact property</li>
-          <li>Figure out how to get legal access to it</li>
-          <li>Decide on your preferred intervention</li>
-        </ol>
-
+        <h2 className="text-2xl font-bold mt-8">
+          No Warranties and Limitation of Liability
+        </h2>
         <p>
-          In the future, we hope to add more specific information on things like applying for small grants, navigating the legal system, and more.
+          Information provided on this website is provided “as is” without
+          warranty of any kind, either express or implied, including without
+          limitation warranties of merchantability, fitness for a particular
+          purpose, or non-infringement. Clean & Green Philly periodically adds,
+          changes, improves, updates or removes information and documents on
+          this website without notice, and assumes no liability or
+          responsibility for any errors or omissions in the contents of this
+          website. YOUR USE OF THIS WEBSITE IS AT YOUR OWN RISK.
         </p>
 
+        <h2 className="text-2xl font-bold mt-8">Linking to Other Websites</h2>
+        <p>
+          When using cleanandgreenphilly.org, you may be directed to content and
+          websites that are not contained within our website. External websites
+          are governed by their own terms and conditions of use, and you agree
+          that you recognize that Clean & Green Philly takes no responsibility
+          for the contents of other linked websites, including those provided by
+          the government. External websites linked to from
+          cleanandgreenphilly.org are not under the control of Clean & Green
+          Philly, and we are not responsible for any information or other links
+          contained in a linked site. These links are provided only for your
+          convenience. Inclusion of any link does not imply recommendation,
+          approval, or endorsement of that website by Clean & Green Philly, nor
+          does it imply that the linked site recommends, approves of, or
+          endorses cleanandgreenphilly.org.
+        </p>
+
+        <h2 className="text-2xl font-bold mt-8">Fair Use Notice</h2>
+        <p>
+          This site contains copyrighted material the use of which has not
+          always been specifically authorized by the copyright owner. In accord
+          with our nonprofit mission, we are making such material available to
+          mitigate gun violence in Philadelphia, Pennsylvania. We believe this
+          constitutes a 'fair use' of any such copyrighted material as provided
+          in Section 107 of the US Copyright Law. In accordance with Title 17
+          U.S.C. Section 107, the material on this site is distributed without
+          profit to those who have expressed a prior interest in receiving the
+          included information for research and educational purposes. For
+          further information on fair use, go to:
+          http://www4.law.cornell.edu/uscode/html/uscode17/usc_sec_17_00000107----000-.html.
+          If you wish to use copyrighted material from this site for purposes of
+          your own that go beyond fair use, you must obtain permission from the
+          copyright owner.
+        </p>
       </div>
     </div>
   );
