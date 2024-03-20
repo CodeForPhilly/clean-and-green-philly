ce23fa256356b68305261af39be7bac18582c7aa
Joseph <joeoani@gmail.com>
2024-03-20 18:20:50 -0400

    Semantic updates that will help screenreader users + keyboard navigation.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,61 +5,70 @@ import Link from "next/link";
 import IconLink from "./IconLink";
 import MobileNav from "./MobileNav";
 
-// Apply multiple CSS classes using CSS Modules
-// const buttonClasses = `${styles.button} ${primary ? styles.primary : ""} ${
-//   large ? styles.large : ""
-// }`;
-
 const Header = () => (
-  <Navbar maxWidth="full" position="sticky" height="auto" isBordered>
-    <MobileNav />
-    <NavbarContent
-      className="hidden sm:flex basis-1/5 sm:basis-full"
-      style={{
-        paddingTop: "16px",
-        paddingBottom: "16px",
-        paddingLeft: "32px"
-      }}
-      justify="start"
-    >
-      <NavbarBrand>
-        <Link href="/">
-          <Image
-            src="/logo.svg"
-            alt="Clean & Green Philly Logo"
-            width={112}
-            height={67}
-          />
-        </Link>
-      </NavbarBrand>
-    </NavbarContent>
+  <header>
+    <Navbar maxWidth="full" position="sticky" height="auto" isBordered>
+      <MobileNav />
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        style={{
+          paddingTop: "16px",
+          paddingBottom: "16px",
+          paddingLeft: "32px",
+        }}
+        justify="start"
+      >
+        <NavbarBrand>
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="Clean & Green Philly Logo"
+              width={112}
+              height={67}
+            />
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
 
-    <NavbarContent
-      className="hidden sm:flex basis-1/5 sm:basis-full"
-      justify="end"
-    >
-      <IconLink
-        icon={<PiBinoculars className="h-6 w-6" />}
-        text="Find Properties"
-        href="/map"
-      />
-      <IconLink
-        icon={<PiKey className="h-6 w-6" />}
-        text="Get Access"
-        href="/get-access"
-      />
-      <IconLink
-        icon={<PiTree className="h-6 w-6" />}
-        text="Transform"
-        href="/transform-property"
-      />
-      <IconLink
-        icon={<PiInfo className="h-6 w-6" />}
-        text="About"
-        href="/about"
-      />
-    </NavbarContent>
-  </Navbar>
+      <nav role="navigation" aria-label="Primary Navigation">
+        <NavbarContent
+          className="hidden sm:flex basis-1/5 sm:basis-full"
+          justify="end"
+        >
+          <ul className="flex space-x-4">
+            <li>
+              <IconLink
+                icon={<PiBinoculars className="h-6 w-6" />}
+                text="Find Properties"
+                href="/map"
+              />
+            </li>
+            <li>
+              <IconLink
+                icon={<PiKey className="h-6 w-6" />}
+                text="Get Access"
+                href="/get-access"
+              />
+            </li>
+            <li>
+              <IconLink
+                icon={<PiTree className="h-6 w-6" />}
+                text="Transform"
+                href="/transform-property"
+              />
+            </li>
+            <li>
+              <IconLink
+                icon={<PiInfo className="h-6 w-6" />}
+                text="About"
+                href="/about"
+              />
+            </li>
+          </ul>
+        </NavbarContent>
+      </nav>
+    </Navbar>
+  </header>
 );
 
 export default Header;
