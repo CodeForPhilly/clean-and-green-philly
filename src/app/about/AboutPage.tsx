dcb65700f89b0f39076502613a02e22acc8b9b69
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-03-21 11:56:39 -0400

    Imported the graphics for Methodology and Vision

--- src/app/about/AboutPage.tsx
+++ src/app/about/AboutPage.tsx
@@ -77,7 +77,7 @@ export default function AboutPage() {
               />
             </div>
           </div>
-          <div className="flex grid grid-cols-2 ">
+          <div className="flex grid grid-cols-2 my-2">
             <div className=" flex flex-col justify-center pr-3">
               <h2 className="heading-2xl font-semibold mb-4">The Challenge</h2>
               <p className="body-md mb-4">
@@ -101,132 +101,178 @@ export default function AboutPage() {
               />
             </div>
           </div>
-          <h2 className="heading-2xl font-semibold mb-4">Our Vision</h2>
-          <p className="body-md mb-4">
-            Our vision was to empower local residents, non-profit organizations,
-            and government stakeholders to find and prioritize vacant properties
-            for interventions, understand how to transform the properties
-            they’ve identified, and connect users to resources that can support
-            them.
-          </p>
-          <h2 className="heading-2xl font-semibold mb-4">Our Methodology</h2>
-          <p className="body-md mb-4">
-            We created a dataset based on the original research conducted by Dr.
-            Eugenia South and her colleagues, as well as many conversations with
-            stakeholders, including community residents, CDCs, City government
-            offices, academic researchers, and more. We analyzed and combined
-            many data sets on crime, green-space, and properties.
-          </p>
-          <Button
-            href="/methodology"
-            as={Link}
-            className="bg-gray-200 iconLink"
-          >
-            <span className="body-md">Learn How</span>
-            <PiArrowRight className="w-5 h-5" />
-          </Button>
-        </div>
-
-        <div>
-          <h2 className="heading-2xl font-semibold mb-4">Contributors</h2>
-          <p className="body-md mb-4">
-            Clean & Green Philly was built by a team of Code for Philly
-            volunteers. The project was created and led by{" "}
-            <a
-              href="https://www.nlebovits.github.io"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="link"
-            >
-              Nissim Lebovits
-            </a>
-            .{" "}
-            <a
-              href="https://www.willonabike.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="link"
-            >
-              Will Budreau
-            </a>{" "}
-            was responsible for user testing and research,{" "}
-            <a
-              href="https://www.brandonfcohen.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="link"
-            >
-              Brandon Cohen
-            </a>{" "}
-            was the lead developer, and{" "}
-            <a
-              href="https://www.nathanielsidwell.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="link"
-            >
-              {" "}
-              Nathaniel Sidwell
-            </a>{" "}
-            led the UX team. Thanks, too, to the many other contributors along
-            the way.
-          </p>
-          <p className="body-md mb-4">
-            Our efforts have been informed and advanced by local residents,
-            community leaders, City staff, faculty at the University of
-            Pennsylvania, Temple University, and Thomas Jefferson University;
-            and many others. Special thanks are due to the Code for Philly
-            leadership, Jon Geeting, Dante Leonard, Mjumbe Poe, and Vicky Tam.
-          </p>
-          <p className="body-md">
-            Lastly, we are grateful to Dr. Eugenia South and her colleagues,
-            whose work this project depends on.
-          </p>
-          <br></br>
-          <h2 className="heading-2xl font-semibold mb-4">Feedback</h2>
-          <p className="body-md mb-4">
-            If you find issues in this website or would like to offer us
-            feedback, please reach out to us at {" "}
-            <a
-              href="mailto:cleanandgreenphl@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="link"
-            >
-              cleanandgreenphl@gmail.com.
-            </a>
-             {" "}
-          </p>
-          <h2 className="heading-2xl font-semibold mb-4">Concerns</h2>
-          <p className="body-md mb-4">
-            Our goal is to help community groups reduce violence through
-            cleaning and greening methods in their neighborhoods. We recognize
-            that may inadvertently increase property values, increase rental
-            prices and displace already-marginalized Philadelphians. We are
-            working to avoid this by meeting extensively with community groups
-            and responding to their concerns. Read more about this topic here.  {" "}
-          </p>
-          <h2 className="heading-2xl font-semibold mb-4">
-            Removing Properties
-          </h2>
-          <p className="body-md mb-4">
-            If you find issues If you would like to request that we remove a
-            property from the dashboard, please see our 
-            <a href="/request-removal" className="link">
-              Request Removal
-            </a>{" "}
-            page.in this website or would like to offer us feedback, please
-            reach out to us at 
-            <a
-              href="mailto:cleanandgreenphl@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="link"
-            >
-              cleanandgreenphl@gmail.com.
-            </a>{" "}
-             {" "}
-          </p>
+          <div className="flex grid grid-cols-2 my-2">
+            <div className=" flex flex-col justify-center pr-3">
+              <h2 className="heading-2xl font-semibold mb-4">Our Vision</h2>
+              <p className="body-md mb-4">
+                Our vision was to empower local residents, non-profit
+                organizations, and government stakeholders to find and
+                prioritize vacant properties for interventions, understand how
+                to transform the properties they’ve identified, and connect
+                users to resources that can support them.
+              </p>
+            </div>
+            <div className="m-4 flex items-center justify-center overflow-hidden">
+              <Image
+                src="/about_vision_graphic.png"
+                alt="A graphic of a cluster of green circles with icons representing aspects of the project"
+                className="border-1"
+                style={{
+                  width: "500px",
+                  height: `${(500 / 16) * 9}px`,
+                }}
+              />
+            </div>
+          </div>
+          <div className="flex grid grid-cols-2 mt-2 mb-6">
+            <div className=" flex flex-col justify-center pr-3">
+              <h2 className="heading-2xl font-semibold mb-4">
+                Our Methodology
+              </h2>
+              <p className="body-md mb-4">
+                We created a dataset based on the original research conducted by
+                Dr. Eugenia South and her colleagues, as well as many
+                conversations with stakeholders, including community residents,
+                CDCs, City government offices, academic researchers, and more.
+                We analyzed and combined many data sets on crime, green-space,
+                and properties.
+              </p>
+              <div className="flex items-center">
+                <Button
+                  href="/methodology"
+                  as={Link}
+                  className="bg-gray-200 iconLink"
+                >
+                  <span className="body-md">Learn More</span>
+                  <PiArrowRight className="w-5 h-5" />
+                </Button>
+              </div>
+            </div>
+            <div className="m-4 flex items-center justify-center overflow-hidden">
+              <Image
+                src="/about_method_graphic.png"
+                alt="A graphic of a green circle with a grid symbol surrounded by other grid symbols with arrows pointing back to the circle."
+                className="border-1"
+                style={{
+                  width: "500px",
+                  height: `${(500 / 16) * 9}px`,
+                }}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="my-8">
+              <h2 className="heading-2xl font-semibold mb-4">Contributors</h2>
+              <p className="body-md mb-4">
+                Clean & Green Philly was built by a team of Code for Philly
+                volunteers. The project was created and led by{" "}
+                <a
+                  href="https://www.nlebovits.github.io"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  Nissim Lebovits
+                </a>
+                .{" "}
+                <a
+                  href="https://www.willonabike.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  Will Budreau
+                </a>{" "}
+                was responsible for user testing and research,{" "}
+                <a
+                  href="https://www.brandonfcohen.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  Brandon Cohen
+                </a>{" "}
+                was the lead developer, and{" "}
+                <a
+                  href="https://www.nathanielsidwell.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  {" "}
+                  Nathaniel Sidwell
+                </a>{" "}
+                led the UX team. Thanks, too, to the many other contributors
+                along the way.
+              </p>
+              <p className="body-md mb-4">
+                Our efforts have been informed and advanced by local residents,
+                community leaders, City staff, faculty at the University of
+                Pennsylvania, Temple University, and Thomas Jefferson
+                University; and many others. Special thanks are due to the Code
+                for Philly leadership, Jon Geeting, Dante Leonard, Mjumbe Poe,
+                and Vicky Tam.
+              </p>
+              <p className="body-md">
+                Lastly, we are grateful to Dr. Eugenia South and her colleagues,
+                whose work this project depends on.
+              </p>
+            </div>
+            <div className="my-6">
+              <h2 className="heading-2xl font-semibold mb-4">Feedback</h2>
+              <p className="body-md mb-4">
+                If you find issues in this website or would like to offer us
+                feedback, please reach out to us at {" "}
+                <a
+                  href="mailto:cleanandgreenphl@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  cleanandgreenphl@gmail.com.
+                </a>
+                 {" "}
+              </p>
+            </div>
+            <div className="my-6">
+              <h2 className="heading-2xl font-semibold mb-4">Concerns</h2>
+              <p className="body-md mb-4">
+                Our goal is to help community groups reduce violence through
+                cleaning and greening methods in their neighborhoods. We
+                recognize that may inadvertently increase property values,
+                increase rental prices and displace already-marginalized
+                Philadelphians. We are working to avoid this by meeting
+                extensively with community groups and responding to their
+                concerns.
+                {/* <a href="/gentrification" className="link"> 
+                Not sure what the path will be but this will go to the Concerns or Gentrification page once it's made. */}
+                Read more about this topic here.   {/* </a> */}
+              </p>
+            </div>
+            <div className="my-6">
+              <h2 className="heading-2xl font-semibold mb-4">
+                Removing Properties
+              </h2>
+              <p className="body-md mb-4">
+                If you find issues If you would like to request that we remove a
+                property from the dashboard, please see our 
+                <a href="/request-removal" className="link">
+                  Request Removal
+                </a>{" "}
+                page.in this website or would like to offer us feedback, please
+                reach out to us at 
+                <a
+                  href="mailto:cleanandgreenphl@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  cleanandgreenphl@gmail.com.
+                </a>{" "}
+                 {" "}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
