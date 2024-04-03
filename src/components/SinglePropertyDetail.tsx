6601201fcf943f8e64a1341327095e9b7edc6b08
Paul H Choi <paulhchoi+github@pm.me>
2024-04-03 19:05:58 -0400

    feat(395): make property selection back button sticky

--- src/components/SinglePropertyDetail.tsx
+++ src/components/SinglePropertyDetail.tsx
@@ -68,15 +68,8 @@ const SinglePropertyDetail = ({
   );
 
   return (
-    <div
-      className="w-full p-4"
-      style={{
-        paddingRight: "24px",
-        paddingBottom: "24px",
-        paddingLeft: "24px",
-      }}
-    >
-      <div className="pb-4">
+    <div className="w-full px-6 pb-6">
+      <div className="sticky top-0 py-4 z-10">
         <Button
           onPress={() => {
             setSelectedProperty(null);
