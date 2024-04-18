cd990aab04ffadabb99de1aedabc7fdb491250d6
Paul H Choi <paulhchoi+github@pm.me>
2024-04-18 15:53:18 -0400

    correcting display logic

--- src/components/PropertyAccessOptionContainer.tsx
+++ src/components/PropertyAccessOptionContainer.tsx
@@ -2,41 +2,41 @@ import { access_options, PropertyAccess } from "@/config/propertyAccessOptions";
 import PropertyAccessOptionCard from "./PropertyAccessOptionCard";
 
 const determineCardEnums = (property: any) => {
+  let best;
+  let neighbor;
   let available = [];
   let unavailable = [];
 
-  const bestCard = () => {
-    switch (property.access_process) {
-      case "Private Land Use Agreement":
-        return PropertyAccess.PRIVATE_LAND_USE;
-      case "Land Bank":
-        return PropertyAccess.LAND_BANK;
-      case "Buy Property":
-        return PropertyAccess.BUY_FROM_OWNER;
-      default:
-        return PropertyAccess.DO_NOTHING;
-    }
-  };
+  property.access_process === "Private Land Use Agreement"
+    ? (best = PropertyAccess.PRIVATE_LAND_USE)
+    : unavailable.push(PropertyAccess.PRIVATE_LAND_USE);
+
+  property.access_process === "Land Bank"
+    ? (best = PropertyAccess.LAND_BANK)
+    : unavailable.push(PropertyAccess.LAND_BANK);
+
+  property.access_process === "Buy Property"
+    ? (best = PropertyAccess.BUY_FROM_OWNER)
+    : property.market_value <= 1000
+    ? available.push(PropertyAccess.BUY_FROM_OWNER)
+    : unavailable.push(PropertyAccess.BUY_FROM_OWNER);
+
+  property.side_yard_eligible === "Yes"
+    ? (neighbor = PropertyAccess.SIDE_YARD)
+    : unavailable.push(PropertyAccess.SIDE_YARD);
 
   property.tactical_urbanism === "Yes"
     ? available.push(PropertyAccess.QUICK_CLEANING)
     : unavailable.push(PropertyAccess.QUICK_CLEANING);
 
-  property.market_value <= 1000
-    ? available.push(PropertyAccess.BUY_FROM_OWNER)
-    : unavailable.push(PropertyAccess.BUY_FROM_OWNER);
-
   property.conservatorship === "Yes"
     ? available.push(PropertyAccess.CONSERVATORSHIP)
     : unavailable.push(PropertyAccess.CONSERVATORSHIP);
 
-  property.access_process !== "Land Bank" &&
-    available.push(PropertyAccess.LAND_BANK);
-
   return {
-    best: bestCard(),
+    best,
     available,
-    neighbor: PropertyAccess.SIDE_YARD,
+    neighbor,
     unavailable,
   };
 };
@@ -47,23 +47,31 @@ const PropertyAccessOptionContainer = ({ property }: any) => {
 
   return (
     <div className="flex flex-col space-y-2">
-      <PropertyAccessOptionCard type={"best"} option={access_options[best]} />
-      {available.map((opt: PropertyAccess) => (
+      {best && (
+        <PropertyAccessOptionCard type={"best"} option={access_options[best]} />
+      )}
+      {available.length > 0 &&
+        available.map((opt: PropertyAccess) => (
+          <PropertyAccessOptionCard
+            key={opt}
+            type={"available"}
+            option={access_options[opt]}
+          />
+        ))}
+      {neighbor && (
         <PropertyAccessOptionCard
-          type={"available"}
-          option={access_options[opt]}
+          type={"neighbors"}
+          option={access_options[neighbor]}
         />
-      ))}
-      <PropertyAccessOptionCard
-        type={"neighbors"}
-        option={access_options[neighbor]}
-      />
-      {unavailable.map((opt: PropertyAccess) => (
-        <PropertyAccessOptionCard
-          type={"unavailable"}
-          option={access_options[opt]}
-        />
-      ))}
+      )}
+      {unavailable.length > 0 &&
+        unavailable.map((opt: PropertyAccess) => (
+          <PropertyAccessOptionCard
+            key={opt}
+            type={"unavailable"}
+            option={access_options[opt]}
+          />
+        ))}
     </div>
   );
 };
