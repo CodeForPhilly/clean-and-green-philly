aec9bd4c98fadd3a04ee377f536e0775355aa714
Jim Grimes <jimgrimes86@gmail.com>
2024-04-18 08:02:15 -0400

    Created useEffect for setting selectedProperty after search; revereted handleMapClick to original function; added state for searchedProperty with type SearchedProperty

--- src/app/find-properties/[[...opa_id]]/page.tsx
+++ src/app/find-properties/[[...opa_id]]/page.tsx
@@ -214,6 +214,7 @@ const MapPage = ({ params }: MapPageProps) => {
               </div>
               {isLinkedPropertyParsed ? (
                 <PropertyMap
+                  featuresInView={featuresInView}
                   setFeaturesInView={setFeaturesInView}
                   setLoading={setLoading}
                   selectedProperty={selectedProperty}
