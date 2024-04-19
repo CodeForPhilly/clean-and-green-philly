54defcf12fa657eb8985d4bacc4a9240bd350d9c
Nico Zigouras <zigouras@upenn.edu>
2024-04-18 23:47:15 -0400

    config and doc changes for a more robusy backend setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,3 +8,7 @@ export const maptilerApiKey =
   process.env.NEXT_PUBLIC_MAPTILER_KEY || EMPTY_STRING;
 
 export const useStagingTiles = false;
+
+export const googleCloudBucketName =
+  process.env.GOOGLE_CLOUD_BUCKET_NAME || "cleanandgreenphl";
+
