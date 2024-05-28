2e01b258f1d5f00873259e99eb75af876cca1718
Gary Pang <gary.pang@gmail.com>
2024-05-28 00:37:31 -0400

    Share CookieContext across UI application

--- src/app/(content-pages)/layout.tsx
+++ src/app/(content-pages)/layout.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import { Footer, Header, Hotjar } from "@/components";
+import { CookieProvider } from "@/context/CookieContext";
 
 const ContentPagesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
-      <main id="main">
-        <div className="max-w-[68.75rem] mx-auto pb-[30px] pt-[60px] w-[90%]">
+      <CookieProvider>
+        <Header />
+        <main id="main">
+          <div className="max-w-[68.75rem] mx-auto pb-[30px] pt-[60px] w-[90%]">
             {children}
-        </div>
-      </main>
-      <Footer />
-      <Hotjar />
+          </div>
+        </main>
+        <Footer />
+        <Hotjar />
+      </CookieProvider>
     </>
   );
 };
