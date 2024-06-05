38bbb61cad4ae3bfd6e429ac9ee41a3927c1da25
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2024-06-04 20:29:04 -0400

    add util to calculate parcel development risk

--- data/src/script.py
+++ data/src/script.py
@@ -8,6 +8,7 @@ from data_utils.city_owned_properties import city_owned_properties
 from data_utils.community_gardens import community_gardens
 from data_utils.conservatorship import conservatorship
 from data_utils.deliquencies import deliquencies
+from data_utils.dev_probability import dev_probability
 from data_utils.drug_crimes import drug_crimes
 from data_utils.gun_crimes import gun_crimes
 from data_utils.imm_dang_buildings import imm_dang_buildings
@@ -33,24 +34,25 @@ if awkde_path not in sys.path:
     sys.path.append(awkde_path)
 
 services = [
-    city_owned_properties,
-    phs_properties,
-    l_and_i,
-    rco_geoms,
-    tree_canopy,
-    nbhoods,
-    gun_crimes,
-    drug_crimes,
-    deliquencies,
-    opa_properties,
-    unsafe_buildings,
-    imm_dang_buildings,
-    tactical_urbanism,
-    conservatorship,
-    llc_owner,
-    community_gardens,
-    park_priority,
-    ppr_properties
+    # city_owned_properties,
+    # phs_properties,
+    # l_and_i,
+    # rco_geoms,
+    # tree_canopy,
+    # nbhoods,
+    # gun_crimes,
+    # drug_crimes,
+    # deliquencies,
+    # opa_properties,
+    # unsafe_buildings,
+    # imm_dang_buildings,
+    # tactical_urbanism,
+    # conservatorship,
+    # llc_owner,
+    # community_gardens,
+    # park_priority,
+    # ppr_properties,
+    dev_probability,
 ]
 
 # backup sql schema if we are reloading data
