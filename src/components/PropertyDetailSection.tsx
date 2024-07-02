b656f32047a3ebb5f73b9fb5e3fa32b82ca8d383
Amy Yeung <amyyeung17@gmail.com>
2024-07-02 12:05:09 -0400

    Replaced class and updated location of div

--- src/components/PropertyDetailSection.tsx
+++ src/components/PropertyDetailSection.tsx
@@ -192,7 +192,6 @@ const PropertyDetailSection: FC<PropertyDetailSectionProps> = ({
   ) : (
     <>
       <div className="flex flex-wrap flex-grow h-full min-h-[calc(100svh-101px)] max-h-[calc(100svh-101px)] mt-2">
-        <div aria-live="polite" className="absolute left-[-9999px]" role="status"> You are on page {page} </div>
         {display === "list" ? (
           <Table
             aria-label="Property Details"
@@ -228,13 +227,16 @@ const PropertyDetailSection: FC<PropertyDetailSectionProps> = ({
             </TableBody>
           </Table>
         ) : (
-          items.map((feature, index) => (
-            <PropertyCard
-              feature={feature}
-              key={index}
-              setSelectedProperty={setSelectedProperty}
-            />
-          ))
+          <>
+            <div aria-live="polite" className="sr-only"> {`You are on page ${page}`} </div>
+            {items.map((feature, index) => (
+              <PropertyCard
+                feature={feature}
+                key={index}
+                setSelectedProperty={setSelectedProperty}
+              />
+            ))}
+          </>
         )}
         {featuresInView?.length > 0 && widthRef.current && (
           <div>
