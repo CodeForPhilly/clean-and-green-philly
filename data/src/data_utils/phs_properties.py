5ab79c7de927ede76050944bd76bc879eeb704b3
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-10-18 17:47:29 -0400

    update function to reflect changes to underlying City dataset; add docstrings + typing

--- data/src/data_utils/phs_properties.py
+++ data/src/data_utils/phs_properties.py
@@ -1,23 +1,33 @@
 from classes.featurelayer import FeatureLayer
 from constants.services import PHS_LAYERS_TO_LOAD
 
+def phs_properties(primary_featurelayer: FeatureLayer) -> FeatureLayer:
+    """
+    Perform a spatial join between the primary feature layer and the PHS properties layer,
+    then update the primary feature layer with a new column 'phs_care_program' indicating
+    if the property is part of the PHS care program.
 
-def phs_properties(primary_featurelayer):
+    Args:
+        primary_featurelayer (FeatureLayer): The primary feature layer to join with the PHS properties layer.
+
+    Returns:
+        FeatureLayer: The updated primary feature layer with the 'phs_care_program' column.
+    """
+    
     phs_properties = FeatureLayer(
-        name="PHS Properties", esri_rest_urls=PHS_LAYERS_TO_LOAD, cols=["BRT_ID"]
+        name="PHS Properties", esri_rest_urls=PHS_LAYERS_TO_LOAD, cols=["program"]
     )
 
-    phs_properties.gdf.loc[:, "phs_partner_agency"] = "PHS"
+    # Perform spatial join between primary feature layer and PHS properties
+    primary_featurelayer.spatial_join(phs_properties)
 
-    primary_featurelayer.opa_join(
-        phs_properties.gdf,
-        "brt_id",
-    )
+    # Initialize 'phs_care_program' column with default "no" for all rows
+    primary_featurelayer.gdf["phs_care_program"] = "no"
+    
+    # Set 'phs_care_program' to "yes" for matched rows
+    primary_featurelayer.gdf.loc[primary_featurelayer.gdf["phs_care_program"] != "no", "phs_care_program"] = "yes"
 
-    primary_featurelayer.gdf.loc[:, "phs_partner_agency"] = primary_featurelayer.gdf[
-        "phs_partner_agency"
-    ].fillna("None")
-
+    # Rebuild the GeoDataFrame after updates
     primary_featurelayer.rebuild_gdf()
 
     return primary_featurelayer
