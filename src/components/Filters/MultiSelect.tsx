c76d4299e296f64bc62edab312c3a1e206d7d6da
Donovan <donovanandrews300@gmail.com>
2025-01-02 14:24:42 -0500

    removed searchable prop

--- src/components/Filters/MultiSelect.tsx
+++ src/components/Filters/MultiSelect.tsx
@@ -33,7 +33,6 @@ const MultiSelect: FC<MultiSelectProps> = ({
       <SelectFilter
         aria-describedby={aria_describedby_label}
         items={options}
-        searchable
         variant="flat"
         size="md"
         radius="md"
