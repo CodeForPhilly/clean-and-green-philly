3125fae636ef8209ee851a0d35290048be37c50f
Adam Levin <zev105@gmail.com>
2025-03-27 12:07:29 -0400

    feat: allow non-credentialed users and cloud env credentials
    Allow creating db url by specifing port and password (to be used in Cloud Run
    to avoid duplicate secret management of POSTGRES_PASSWORD and VACANT_LOTS_DB)
    
    Add additional logic to psql to user localhost in Cloud Run environments
    
    Add a new bucket manager that should work in Cloud Run, with file credentials
    and in read-only mode for non-credentialed users
    
    Changed slack reporters to not report with a message rather than
    erroring if no credentials exist

--- data/src/test/test_slack_error_reporter.py
+++ data/src/test/test_slack_error_reporter.py
@@ -5,17 +5,18 @@ from unittest.mock import patch
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
-from classes.slack_error_reporter import (
+from new_etl.classes.slack_reporters import (
     send_error_to_slack,
 )  # Ensure correct file import
 
 
 class TestSlackNotifier(unittest.TestCase):
     @patch(
-        "classes.slack_error_reporter.WebClient.chat_postMessage"
+        "new_etl.classes.slack_reporters.WebClient.chat_postMessage"
     )  # Correct patching
     @patch(
-        "classes.slack_error_reporter.os.getenv", return_value="mock_slack_token"
+        "new_etl.classes.slack_reporters.os.getenv",
+        return_value="mock_slack_token",
     )  # Correct patching
     def test_send_error_to_slack(self, _mock_getenv, mock_slack_post):
         """Test that Slack error reporting is triggered correctly."""
@@ -23,7 +24,7 @@ class TestSlackNotifier(unittest.TestCase):
         error_message = "Test error message"
 
         # Call the Slack notification function
-        send_error_to_slack(error_message)
+        send_error_to_slack(error_message, slack_token="test_token")
 
         # Verify the Slack API call was made with the correct parameters
         mock_slack_post.assert_called_once_with(
@@ -33,18 +34,13 @@ class TestSlackNotifier(unittest.TestCase):
         )
 
     @patch(
-        "classes.slack_error_reporter.WebClient.chat_postMessage"
+        "new_etl.classes.slack_reporters.WebClient.chat_postMessage"
     )  # Correct patching
     @patch(
-        "classes.slack_error_reporter.os.getenv", return_value=None
+        "new_etl.classes.slack_reporters.os.getenv", return_value=None
     )  # Simulate missing Slack token
     def test_no_error_no_slack_message(self, _mock_getenv, mock_slack_post):
         """Test that Slack notification is not triggered if there's no error."""
-
-        # Call the Slack notification function (with no valid token)
-        with self.assertRaises(ValueError):
-            send_error_to_slack("Test error message")
-
         # Ensure Slack's chat_postMessage was not called due to missing token
         mock_slack_post.assert_not_called()
 
