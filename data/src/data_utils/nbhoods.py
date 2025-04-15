31e07bde9159949efc2361a8ebdef57a10ef80c1
Graham Beckley <gbeckley@mozilla.com>
2025-04-15 16:36:00 -0400

    fix: format files with ruff

--- data/src/data_utils/nbhoods.py
+++ data/src/data_utils/nbhoods.py
@@ -7,19 +7,19 @@ from config.config import USE_CRS
 
 def nbhoods(primary_featurelayer):
     phl_nbhoods = gpd.read_file(NBHOODS_URL)
-    
+
     # Correct the column name to uppercase if needed
-    if 'MAPNAME' in phl_nbhoods.columns:
+    if "MAPNAME" in phl_nbhoods.columns:
         phl_nbhoods.rename(columns={"MAPNAME": "neighborhood"}, inplace=True)
-    
+
     phl_nbhoods = phl_nbhoods.to_crs(USE_CRS)
-    
+
     nbhoods = FeatureLayer("Neighborhoods")
     nbhoods.gdf = phl_nbhoods
-    
+
     red_cols_to_keep = ["neighborhood", "geometry"]
     nbhoods.gdf = nbhoods.gdf[red_cols_to_keep]
-    
+
     primary_featurelayer.spatial_join(nbhoods)
-    
+
     return primary_featurelayer
