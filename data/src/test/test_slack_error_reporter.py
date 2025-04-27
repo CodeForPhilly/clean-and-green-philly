d802e51a5a5b296d3b219666c636d0a4e65cf2d4
Adam Levin <zev105@gmail.com>
2025-04-27 15:47:59 -0400

    chore: made credentials optional for current/old pipeline
    The pipeline can be run locally but will not write changes to the bucket
    and will not report errors to slack without credentials. Previously, the
    code would error when credentials were missing.
    
    Added BACKUP_SCHEMA=False and made FORCE_RELOAD=True as the default.
    These are temporary fixes and will not be relevant once postgres is
    removed.

--- data/src/test/test_slack_error_reporter.py
+++ data/src/test/test_slack_error_reporter.py
@@ -5,17 +5,17 @@ from unittest.mock import patch
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
-from new_etl.classes.slack_reporters import (
-    send_error_to_slack,
-)  # Ensure correct file import
+from classes.slack_error_reporter import (
+    send_error_to_slack,  # Ensure correct file import
+)
 
 
 class TestSlackNotifier(unittest.TestCase):
     @patch(
-        "new_etl.classes.slack_reporters.WebClient.chat_postMessage"
+        "classes.slack_error_reporter.WebClient.chat_postMessage"
     )  # Correct patching
     @patch(
-        "new_etl.classes.slack_reporters.os.getenv",
+        "classes.slack_error_reporter.os.getenv",
         return_value="mock_slack_token",
     )  # Correct patching
     def test_send_error_to_slack(self, _mock_getenv, mock_slack_post):
@@ -34,10 +34,10 @@ class TestSlackNotifier(unittest.TestCase):
         )
 
     @patch(
-        "new_etl.classes.slack_reporters.WebClient.chat_postMessage"
+        "classes.slack_error_reporter.WebClient.chat_postMessage"
     )  # Correct patching
     @patch(
-        "new_etl.classes.slack_reporters.os.getenv", return_value=None
+        "classes.slack_error_reporter.os.getenv", return_value=None
     )  # Simulate missing Slack token
     def test_no_error_no_slack_message(self, _mock_getenv, mock_slack_post):
         """Test that Slack notification is not triggered if there's no error."""
