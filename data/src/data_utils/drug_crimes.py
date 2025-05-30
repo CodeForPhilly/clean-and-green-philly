a0c4dc2af1943811c989f8407fcb4e05abdb2bad
Graham Beckley <graham.beckley@gmail.com>
2025-05-29 20:38:06 -0400

    Remove most code outside of new_etl

--- data/src/data_utils/drug_crimes.py
+++
@@ -1,8 +0,0 @@
-from src.constants.services import DRUGCRIME_SQL_QUERY
-from src.data_utils.kde import apply_kde_to_primary
-
-
-def drug_crimes(primary_featurelayer):
-    return apply_kde_to_primary(
-        primary_featurelayer, "Drug Crimes", DRUGCRIME_SQL_QUERY
-    )
--- data/src/data_utils/gun_crimes.py
+++
@@ -1,6 +0,0 @@
-from src.constants.services import GUNCRIME_SQL_QUERY
-from src.data_utils.kde import apply_kde_to_primary
-
-
-def gun_crimes(primary_featurelayer):
-    return apply_kde_to_primary(primary_featurelayer, "Gun Crimes", GUNCRIME_SQL_QUERY)
