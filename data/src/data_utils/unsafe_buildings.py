d6a69d87f36a7716f98343fb8812c8103a8beaee
Collum Freedman <collum.freedman@gmail.com>
2025-06-10 09:52:07 -0400

    undo change for old pipeline code

--- data/src/data_utils/unsafe_buildings.py
+++ data/src/data_utils/unsafe_buildings.py
@@ -5,7 +5,7 @@ from src.constants.services import UNSAFE_BUILDINGS_QUERY
 def unsafe_buildings(primary_featurelayer):
     unsafe_buildings = FeatureLayer(
         name="Unsafe Buildings",
-        carto_queries=UNSAFE_BUILDINGS_QUERY,
+        carto_sql_queries=UNSAFE_BUILDINGS_QUERY,
         use_wkb_geom_field="the_geom",
         cols=["opa_account_num"],
     )
